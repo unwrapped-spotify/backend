cd7e8e8b59da3b7b02f77c88c8f998a4aad373cd
George Desktop <georgecaws@gmail.com>
2021-12-17 18:19:16 +0000

    Using mux router

--- main.go
+++ main.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func healthcheckCall(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "{success: true}")
+	fmt.Fprintf(writer, "{alive: true}")
 	fmt.Println("Endpoint Hit: healthcheck")
 }
 
 func handleRequests() {
+	// Create a router using the mux library
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", healthcheckCall)
+
 	http.HandleFunc("/healthcheck", healthcheckCall)
 	log.Fatal(http.ListenAndServe(":500", nil))
 }
 
 func main() {
+	fmt.Println("RESTful Go API starting on ")
 	handleRequests()
 }
